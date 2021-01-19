822f8612cf113ef27189c055de21445043ef1f44
jason <jasonzhang666@163.com>
2021-01-20 00:45:03 +0800

    hystrix 超时降级处理 * @HystrixCommand * @EnableCircuitBreaker

--- cloud-provider-hystrix-payment8001/src/main/java/com/zhangxujie/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/zhangxujie/springcloud/service/PaymentService.java
@@ -6,6 +6,8 @@
  */
 package com.zhangxujie.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -13,20 +15,38 @@ import java.util.concurrent.TimeUnit;
 @Service
 public class PaymentService {
 
-    public String paymentInfo_OK(Integer id){
+    public String paymentInfo_OK(Integer id) {
 
         return "OK\t线程池：" + Thread.currentThread().getName() + "\tpaymentInfo_OK, id: " + id + "\t";
     }
 
-    public String paymentInfo_Timeout(Integer id){
+
+    //fallbackMethod：兜底方法名
+    @HystrixCommand(
+            fallbackMethod = "paymentInfo_TimeoutHandler",
+            commandProperties = {
+                    @HystrixProperty(
+                            name = "execution.isolation.thread.timeoutInMilliseconds",
+                            value = "3000"//峰值 3s为上限，超过3s为超时，启用兜底方法
+                    )
+            }
+    )
+    public String paymentInfo_Timeout(Integer id) {
         int t = 5;
         try {
             TimeUnit.SECONDS.sleep(t);
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-        }finally {
+        } finally {
 
         }
         return "Timeout\t线程池：" + Thread.currentThread().getName() + "\tpaymentInfo_Timeout, id: " + id + "\t耗时" + t + "秒。";
     }
+
+    public String paymentInfo_TimeoutHandler(Integer id) {
+        return "Timeout\t线程池：" + Thread.currentThread().getName() + "\t系统繁忙，请稍后由再试！\tHandler, id: " + id + "\t降级处理。";
+
+    }
+
+
 }
